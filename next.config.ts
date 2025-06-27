8307bbe4f20afbc2ae36d4cf5551f0e56def5cf2
Riemann <think4nothing@outlook.com>
2025-06-27 16:43:19 +0800

    finish ch13

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  allowedDevOrigins: ['local-origin.dev', '*.local-origin.dev','10.0.0.3'],
+  /*
+  experimental: {
+    ppr: 'incremental'
+  }
+  */
 };
 
 export default nextConfig;
